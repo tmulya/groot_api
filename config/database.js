f2eda30f94177fce403d2b181289097c95694998
tirtamulya <tmulya@gmail.com>
2021-09-07 15:16:45 +0000

    fix

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'groot_project'),
+        database: env('DATABASE_NAME', 'groot_db'),
         username: env('DATABASE_USERNAME', 'tirta'),
-        password: env('DATABASE_PASSWORD', '1234567890'),
+        password: env('DATABASE_PASSWORD', 'Tirta123'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
